7748e86cf57f8554cd477357aa5c6f7729a2956c
hamuhamu20 <hamu.hamurin05@gmail.com>
2024-10-15 16:38:52 +0900

    遺伝子を時間割事に割り振りました。

--- main2.py
+++ main2.py
@@ -12,6 +12,34 @@ grade2 = 0
 grade3 = 0
 grade4 = 0
 error = 0
+
+day_time_count = {
+    "mon1st": 0,
+    "mon2nd": 0,
+    "mon3rd": 0,
+    "mon4th": 0,
+    "mon5th": 0,
+    "tue1st": 0,
+    "tue2nd": 0,
+    "tue3rd": 0,
+    "tue4th": 0,
+    "tue5th": 0,
+    "wed1st": 0,
+    "wed2nd": 0,
+    "wed3rd": 0,
+    "wed4th": 0,
+    "wed5th": 0,
+    "thu1st": 0,
+    "thu2nd": 0,
+    "thu3rd": 0,
+    "thu4th": 0,
+    "thu5th": 0,
+    "fri1st": 0,
+    "fri2nd": 0,
+    "fri3rd": 0,
+    "fri4th": 0,
+    "fri5th": 0
+}
 def create_genom(length):
     """
     引数で指定された桁のランダムな遺伝子情報を生成、格納したgenomClassで返します。
@@ -32,33 +60,57 @@ def evaluation(ga):
     :param ga: 評価を行うgenomClass
     :return: 評価処理をしたgenomClassを返す
     """
+    fitness = 0
     global grade1, grade2, grade3, grade4, error
+
+    ##遺伝子を学年別に割り振る
     gene2 = ga.getGenom()[2]
     gene3 = ga.getGenom()[3]
     ##print(f"gene2: {gene2}, gene3: {gene3}") 
 
     if gene2 == 0 and gene3 == 0 and grade1 < 244:
-        grade1+=1
+        grade1 += 1
         ##print(f"Grade1 incremented: {grade1}")
-        return 1
+        fitness += 1
     elif gene2 == 0 and gene3 == 1 and grade2 < 95:
-        grade2+=1
+        grade2 += 1
         ##print(f"Grade2 incremented: {grade2}")
-        return 1
+        fitness += 1
     elif gene2 == 1 and gene3 == 0 and grade3 < 82:
-        grade3+=1
+        grade3 += 1
         ##print(f"Grade3 incremented: {grade3}")
-        return 1
-    elif gene2 == 1 and gene3 == 1 and grade4 < 42:
-        grade4+=1
+        fitness += 1
+    elif gene2 == 1 and gene3 == 1 and grade4 < 48:
+        grade4 += 1
         ##print(f"Grade4 incremented: {grade4}")
-        return 1
+        fitness += 1
     else:
-        error+=1
+        error += 1
         ##print("error")
-        return -1
+        fitness -= 1
 
 
+
+    ##遺伝子を時間割ごとに割り振る
+    gene0 = ga.getGenom()[0]
+    gene1 = ga.getGenom()[1]
+    
+
+    day = days(gene0)
+    time = times(gene1)
+
+    day_time = f"{day}{time}"
+    if day_time in day_time_count:
+        if day_time_count[day_time] < 21:
+            fitness += 1 
+            day_time_count[day_time] += 1
+            ##print(day_time_count[day_time]) 
+        else:
+            fitness -= 1
+
+    return fitness
+
+##曜日の値を返す
 def days(gene1):
     if gene1 == 1:
         return 'mon'
@@ -72,7 +124,8 @@ def days(gene1):
         return 'fri'
     else:
         print("error")
-    
+
+##時限の値を返す    
 def times(gene1):
     if gene1 == 1:
         return '1st'
@@ -103,36 +156,6 @@ MAX_GENERATION = 1
 
 
 if __name__ == '__main__':
-
-    day_time_count = {
-        "mon1st": 0,
-        "mon2nd": 0,
-        "mon3rd": 0,
-        "mon4th": 0,
-        "mon5th": 0,
-        "tue1st": 0,
-        "tue2nd": 0,
-        "tue3rd": 0,
-        "tue4th": 0,
-        "tue5th": 0,
-        "wed1st": 0,
-        "wed2nd": 0,
-        "wed3rd": 0,
-        "wed4th": 0,
-        "wed5th": 0,
-        "thu1st": 0,
-        "thu2nd": 0,
-        "thu3rd": 0,
-        "thu4th": 0,
-        "thu5th": 0,
-        "fri1st": 0,
-        "fri2nd": 0,
-        "fri3rd": 0,
-        "fri4th": 0,
-        "fri5th": 0
-    }
-
-
     # 一番最初の現行世代個体集団を生成します。
     current_generation_individual_group = []
     for i in range(MAX_GENOM_LIST):
@@ -145,17 +168,8 @@ if __name__ == '__main__':
             current_generation_individual_group[i].setEvaluation(evaluation_result)
 
 
-    for i in current_generation_individual_group:
-        gene0 = i.getGenom()[0]
-        gene1 = i.getGenom()[1]
+    
         
-
-        day = days(gene0)
-        time = times(gene1)
-
-        day_time = f"{day}{time}"
-        if day_time in day_time_count:
-            day_time_count[day_time] += 1
     
     # 時間割形式で結果を表示する
     print("     | 1st | 2nd | 3rd | 4th | 5th |")
